af0c397dd6f5c4a4e43118ad66ea480194d04cca
FlyingRain <1198420279@qq.com>
2016-08-17 01:24:15 +0800

    add some code

--- myweb/myweb-application/src/main/java/com/wl/applictaion/ApplicationRegister.java
+++ myweb/myweb-application/src/main/java/com/wl/applictaion/ApplicationRegister.java
@@ -1,6 +1,10 @@
 package com.wl.applictaion;
 
 import com.wl.applictaion.containers.ResourceContainer;
+import com.wl.myweb.api.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
 import java.util.HashMap;
@@ -12,8 +16,19 @@ import java.util.Map;
  */
 public class ApplicationRegister {
 
+    private Logger logger = LoggerFactory.getLogger(ApplicationRegister.class);
+
     private Map<String,ApplicationContext> contextContainer = new HashMap();
 
     private ResourceContainer resourceContainer = new ResourceContainer();
+
+    public ApplicationRegister(String module,ApplicationContext applicationContext){
+
+        contextContainer.put(module,applicationContext);
+        logger.info("start find rest resources!");
+        Map<String,Resource> resources = applicationContext.getBeansOfType(Resource.class);
+
+
+    }
 
 }
