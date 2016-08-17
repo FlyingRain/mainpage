ecf693c1031f886d5661d5483dd29b540ea238d8
FlyingRain <1198420279@qq.com>
2016-08-17 23:56:23 +0800

    fix gitIgnore

---
+++ myweb/myweb-basic/src/main/java/com/wl/myweb/basic/conf/BasicConfig.java
@@ -0,0 +1,12 @@
+package com.wl.myweb.basic.conf;
+
+
+
+/**
+ * Created by Administrator on 2016/8/17.
+ */
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BasicConfig {
+}
