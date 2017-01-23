8bfb01d5cdc5f5c020e46d81c4ee4385c4c588d8
wul <1198420279@qq.com>
2017-01-23 20:41:10 +0800

    modify defualt value of null

--- myweb/myweb-basic/src/main/java/com/wl/myweb/basic/utils/ModelToView.java
+++ myweb/myweb-basic/src/main/java/com/wl/myweb/basic/utils/ModelToView.java
@@ -1,5 +1,8 @@
 package com.wl.myweb.basic.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Field;
 
 /**
@@ -7,16 +10,18 @@ import java.lang.reflect.Field;
  */
 public class ModelToView {
 
+    private static Logger logger = LoggerFactory.getLogger(ModelToView.class);
+
     public static <T> T modelToView(Object object, Class<T> returnType) {
         Field fields[] = object.getClass().getDeclaredFields();
         T temp = null;
         try {
             temp = returnType.newInstance();
         } catch (InstantiationException e) {
-            e.printStackTrace();
+            logger.error("instance object failed!" + object.getClass().getName(),e);
             return null;
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
+            logger.error("can't access object !",e);
             return null;
         }
         Field tempFields[] = returnType.getDeclaredFields();
@@ -24,9 +29,13 @@ public class ModelToView {
             fields[i].setAccessible(true);
             for (int j = 0; j <tempFields.length; j++) {
                 tempFields[j].setAccessible(true);
-                if(tempFields[j].getName().equals(fields[i].getName())){
+                //当声明的域和声明的类型都相同时进行赋值操作
+                if(tempFields[j].getName().equals(fields[i].getName())&&(tempFields[j].getDeclaringClass()==fields[i].getDeclaringClass())){
                     try {
                         tempFields[j].set(temp,fields[i].get(object));
+                        if(fields[i].getDeclaringClass()==String.class&&fields[i].get(object)==null){
+                            tempFields[i].set(temp,"");
+                        }
                     } catch (IllegalAccessException e) {
                         e.printStackTrace();
                         return null;
